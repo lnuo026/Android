8512924693771a61f384b9e3cd6aa26175f3a301
Liang Nuona <lnuo026@aucklanduni.ac.nz>
2026-03-23 11:47:53 +1300

    flastlist / tochableOpacity

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,74 @@
-import { Text, View } from "react-native";
+/**
+ *  FlatList = 列表组件
+ * 
+ * <FlatList
+    data={articles}        // 数据从哪里来
+    keyExtractor={...}     // 每条数据的唯一id
+    renderItem={...}       // 每条数据长什么样
+  />
+ 
+ */  
+import {Text,  View , StyleSheet, FlatList, TouchableOpacity}  from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const articles = [
+    { id: "1", title: "Hammer really likes me", author:
+  "Nora", content: "She is so cute that she does not know it at all." },
+    { id: "2", title: "Nora is really silly", author:
+  "Hammer", content: "She thinks that I don't know I am cute." },
+    { id: "3", title: "We care each other", author: "Nora & Hammer", content: "We can live together forever." },
+  ];
+
+
+  export default function Index(){
+    return(
+          <View style={styles.container}>
+            <Text style={styles.title}>Nora & Hammer's Articles</Text>
+            <Text style={styles.subtitle}>Welcome to our app!</Text>
+
+            <FlatList 
+            data ={articles}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) =>(
+              <TouchableOpacity   style={styles.card} >
+              <Text style={styles.cardTitle}>{item.title}</Text>
+              <Text style={styles.cardAuthor}> by {item.author}</Text>
+              </TouchableOpacity>
+            )}
+            /> 
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:  1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor: "#f7adad",
+    },
+    title:{
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+    subtitle: {
+      fontSize: 16,
+      color: "gray",
+    },
+    card: {
+    backgroundColor: "#abe0c2",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    width: "100%",
+  },
+   cardTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  cardAuthor: {
+    fontSize: 14,
+    color: "red",
+    marginTop: 5,
+  }
+});
